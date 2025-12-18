cb1e96544d8971db665c7fddf184b29150d8bb4d
Jorge Martinez <jotadaniel93@gmail.com>
2025-12-18 08:40:30 -0400

    .

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const IMAGE_BASE_URL = import.meta.env.VITE_API_URL;
+export const IMAGE_BASE_URL = 'https://api.dev.financiados.app';
 
 const axiosInstance = axios.create({
-  baseURL: `${IMAGE_BASE_URL}api/admin`,
+  baseURL: 'https://api.dev.financiados.app/api/admin',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
